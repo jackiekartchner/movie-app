1cec547d4cd0ad3a4899f631366bf1d911fd315b
jackie <jacqueline.kartchner@gmail.com>
2019-09-05 13:14:06 -0700

    adds validations to Actors model

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,7 @@
 class Actor < ApplicationRecord
+
+  validates :first_name, length: { minimum: 2 }
+  validates :last_name, length: { minimum: 2 }
+  validates :known_for, presence: true
+  validates :age, inclusion: { in: %w(age > 13), message: "%{value} is not a valid age" }
 end
