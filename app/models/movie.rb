8ad2367f3eb0117dc297d88a3c4fa1cd76bc6601
jackie <jacqueline.kartchner@gmail.com>
2019-09-05 13:27:07 -0700

    adds validations to movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  validates :title, uniqueness: true
+  validates :year, numericality: true
+  validates :plot, length: { in: 10...500 } 
+  validates :director, presence: true
 end
