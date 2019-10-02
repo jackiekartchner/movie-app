549fc6ab74ef72da0ea28b7bebc7cb66502d8299
jackie <jacqueline.kartchner@gmail.com>
2019-10-02 15:54:50 -0700

    removes validtions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_secure_password
-  validates :email, presence: true, uniqueness: 
+  # has_secure_password
+  # validates :email, presence: true, uniqueness: 
 
   has_many :actors
   has_many :movies 
